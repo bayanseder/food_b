623fe92c33372c5c65f731c5cbb3a08a8642b568
bayanseder <bayansider88@gmail.com>
2020-03-12 17:04:57 +0200

    random function

--- src/app.js
+++ src/app.js
@@ -1,59 +1,98 @@
 const express = require("express");
-const bodyParser =require("body-parser");
+const bodyParser = require("body-parser");
 const compression = require("compression");
-const path =require("path")
+const path = require("path");
 const app = express();
-const errors =require('./controller/error')
-const food = require('./controller/food')
-const request = require('request')
-const foodApi ='https://www.themealdb.com/api/json/v1/1/'
-app.set('port',process.env.PORT||3000);
+const errors = require("./controller/error");
+const food = require("./controller/food");
+const request = require("request");
+const foodApi = "https://www.themealdb.com/api/json/v1/1/";
+app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.disable('x-powered-by');
-app.get('/',(req ,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','index.html'))
+app.disable("x-powered-by");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
-app.get('/food',food);
+const randomurl = "https://www.themealdb.com/api/json/v1/1/random.php";
+//app.get('/food',food);
 
+app.use(
+  express.static(path.join(__dirname, "..", "public"), { maxAge: "30d" })
+);
 
-app.use(express.static(path.join(__dirname,'..','public'),{maxAge : '30d'}));
-
-app.get('/categories',(req ,res)=>{
-    request(foodApi+'categories.php',(err,response,body)=>{
-        if(err){
-            return res.status(500).send(err);
-        }
-        if(body!== null){
-            res.send(body)}
-    })
+app.get("/categories", (req, res) => {
+  request(foodApi + "categories.php", (err, response, body) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    if (body !== null) {
+      res.send(body);
+    }
+  });
 });
-app.get('/categories/:categoryName',(req,res)=>{
-    request(foodApi+`filter.php?c=${req.params.categoryName}`,(err,response,body)=>{
-        if(err){
-            return res.status(500).send(err);
-        }
-        if(body!== null){
-            res.send(body)}
-    })
-})
+app.get("/categories/:categoryName", (req, res) => {
+  request(
+    foodApi + `filter.php?c=${req.params.categoryName}`,
+    (err, response, body) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      if (body !== null) {
+        res.send(body);
+      }
+    }
+  );
+});
 
-app.get('/api/meals/:mealName',(req , res)=>{
-   request(foodApi+`search.php?s=${req.params.mealName}`,(err,response,body)=>{
-    if(err){
-            return res.status(500).send(err);
-        }
-        if(body!== null){
-            res.send(body)} 
-    })
-})
+app.get("/api/meals/:mealName", (req, res) => {
+  request(
+    foodApi + `search.php?s=${req.params.mealName}`,
+    (err, response, body) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      if (body !== null) {
+        res.send(body);
+      }
+    }
+  );
+});
 
-app.get('/meals/:mealName',(req , res)=>{
-    res.sendFile(path.join(__dirname,'..','public','meals.html'));
-})
-
+app.get("/meals/:mealName", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "meals.html"));
+  });
+  
+  app.get("/api/random", (req, res) => {
+    const data = [];
+  
+    food.getRandom()
+      .then(result => {
+        data.push(JSON.parse(result));
+        return food.getRandom();
+      })
+      .then(result => {
+        data.push(JSON.parse(result));
+        return food.getRandom();
+      })
+      .then(result => {
+        data.push(JSON.parse(result));
+        return food.getRandom();
+      })
+      .then(result => {
+        data.push(JSON.parse(result));
+        food.getRandom();
+      })
+      .then(result => {
+        console.log(data);
+      })
+  
+      .catch(err => {
+        console.log(err);
+      });
+  });
 
 app.use(errors.notFound);
 app.use(errors.serverErr);
-app.listen(app.get('port'))
+app.listen(app.get("port"));
--- src/controller/food.js
+++ src/controller/food.js
@@ -1,18 +1,15 @@
 const request = require("request");
+const fetch = require("node-fetch");
 
 
 const getFood =  (req, res) => {
-   console.log("gghggg")
   request(foodApi+`search.php?s=${req.params.mealName}`,(err,response,body)=>{
     if(err){
             return res.status(500).send(err);
         }
         if(body!== null){
-          console.log(body)
             res.send(body)} 
     })
-
-
 
 //    getRequestData(() => {
 
@@ -25,41 +22,22 @@ const getFood =  (req, res) => {
 };
 
 
+const randomurl ='https://www.themealdb.com/api/json/v1/1/random.php'
 
-// async function  tt()
-// {
-//     let eeee =  await request.get(
-//         `https://www.themealdb.com/api/json/v1/1/random.php`,(e,r,b)=>{
+function getRandom() {
+    return new Promise((resolve, reject) => {
+      request(randomurl, (err, response, body) => {
+        if (err) {
+          return reject(err);
+        }
+        try {
+          //resolve(data.push(JSON.parse(body)), console.log("inside", data));
+          resolve(body);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  }
 
-//            // console.log(JSON.parse(b))
-//         })
-
-//         console.log("ttt")
-//         console.log(eeee)
-// }
-
-
-// function getRequestData(cb) {
-//   // console.log(arr)
-//   console.log("inside tiem iut");
-//   request.get(
-//     `https://www.themealdb.com/api/json/v1/1/random.php`,
-//     (err, response, body) => {
-//       const bodyOpject = JSON.parse(body);
-
-//       arr.push({
-//         strMeal: bodyOpject.meals[0].strMeal,
-//         imgsrc: bodyOpject.meals[0].strMealThumb
-//       });
-
-//       console.log(arr);
-//       if (arr.length < 4) {
-//         getRequestData(cb);
-//       } else {
-//         cb();
-//       }
-//     }
-//   );
-// }
-
-module.exports = getFood;
+module.exports ={ getFood ,getRandom};
